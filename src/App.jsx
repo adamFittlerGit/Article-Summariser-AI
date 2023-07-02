06fc785fd4e2e680829470248d6c5d0734b26545
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2023-07-02 22:38:37 +1000

    Colours updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Hero } from "./components/Hero";
-//import { Demo } from "./components/Demo";
+import Hero from './components/Hero';
+
 
 import './App.css';
 
 const App = () => {
-  
   return (
-    <main>
-      <div className='main'>
-        <div className='gradient'>
-          <div className='app'>
-            <Hero />
-          </div>
-        </div>
-      </div>
-    </main>
+    <div>App</div>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,10 @@
-import { logo } from '../assets';
+import React from 'react'
 
 const Hero = () => {
   return (
-    <header className='w-full flex justify-center items-center flex-col'>
-        <nav className='flex justify-between items-center flex-col'>
-            <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
-            <button
-                type="button"
-                onClick={() => window.open('https://github.com/adamFittlerGit')}
-                className='black_btn'
-            >
-                GitHub
-            </button>
-        </nav>
-    </header>
+    <div>Hero</div>
   )
 }
 
 export default Hero
+
