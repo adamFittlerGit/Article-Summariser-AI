31947d766486b28760a9e05ad1cc6f1628af8ef5
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2023-07-26 08:17:20 +1000

    functionality updated
    now it work but looks shit, next job is to fix up the UI

--- src/services/article.js
+++ src/services/article.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
@@ -9,14 +9,17 @@ export const articleApi = createApi({
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
-            return headers;
-        }
-    }),
-    endpoints: (buidler) => ({
-        getSummary: buidler.query({
-            query: (params) => `summerize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-        })
-    })
-});
 
-export const { useLazyGetSummaryQuery } = articleApi;
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
+
+export const { useLazyGetSummaryQuery } = articleApi
